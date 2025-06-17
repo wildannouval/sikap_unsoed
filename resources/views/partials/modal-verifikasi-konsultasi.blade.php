3e264360a6665e1bbdf795e10d9e34e0c8572158
Wildan Nouval Rizki <wildannouvalrizki@gmail.com>
2025-06-17 08:26:54 +0700

    Refactor: z-index modal

--- resources/views/partials/modal-verifikasi-konsultasi.blade.php
+++ resources/views/partials/modal-verifikasi-konsultasi.blade.php
@@ -1,4 +1,4 @@
-<div id="verifikasiKonsultasiModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-[60] justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="verifikasiKonsultasiModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-lg max-h-full"> {{-- Anda bisa ganti max-w-lg menjadi max-w-xl atau max-w-2xl jika butuh lebih lebar --}}
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-800">
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
